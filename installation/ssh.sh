4045064b854866ce9bd5b1a36b5dbcab0cb53ccb
Vitalii Sydorenko <vetal.sydo@gmail.com>
2017-07-23 14:43:16 +0300

    added bin copyssh

--- installation/ssh.sh
+++ installation/ssh.sh
@@ -4,4 +4,4 @@ ssh-keygen -t rsa -b 4096 -C "vetal.sydo@gmail.com" \
     && eval "$(ssh-agent -s)" \
     && ssh-add ~/.ssh/id_rsa \
     && xclip -sel clip < ~/.ssh/id_rsa.pub \
-    && echo "Key is copied ti the memory!"
+    && echo "Key for user \"$USER\" is copied ti the memory!"
