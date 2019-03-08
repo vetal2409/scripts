b1f4a9f0c663941af01f931365d6a4278baf1c90
Vitalii Sydorenko <vetal.sydo@gmail.com>
2019-03-08 17:46:34 +0200

    shutdown

--- installation/docker-compose.sh
+++ installation/docker-compose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Run as sudo user; `sudo -i`
-dockerComposeVersion=1.14.0
+dockerComposeVersion=1.16.1
 
 echo $dockerComposeVersion
 curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
